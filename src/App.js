77886452aa795bfd0fa2034cc34fb4bce5a3b956
Daniel Frota <danielfrot@gmail.com>
2022-11-04 19:16:57 -0400

    atualizando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-target-blank */
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-function App() {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
   const [data, setData] = useState();
   const {
     register,
@@ -17,6 +20,7 @@ function App() {
     try {
       const res = await axios.get(`https://api.github.com/users/${github}`);
       setData(res.data);
+      console.log(res.data)
     } catch (error) {
       if (error) alert("Usuário não encontrado!");
     }
@@ -55,20 +59,28 @@ function App() {
         type="button"
         className="App-button"
         onClick={() => setData(null)}
+        style={{margin: 10}}
       >
         Limpa tela
       </button>
-      <img src={data.avatar_url} alt="foto perfil" className="foto" />
-      <p>{data.name}</p>
-      <a
-        className="App-link"
-        href="https://docs.google.com/forms/d/e/1FAIpQLSfhPSSuCXRZFAoxf4EZDmRoCZVsreYg8uDeLfZwz0x9cTt8pQ/viewform"
-        target="_blank"
-      >
-        Aprenda React
-      </a>
+      <div className="card">
+      <img src={data.avatar_url} alt="foto perfil" className="foto"/>
+        <h3>{data.name}</h3>
+        <p>{data.bio}</p>
+        <table>
+          <tr><td>Github: <a className="App-link" href={data.html_url} target="_blank">{data.html_url}</a></td></tr>
+          <tr><td>Linkedin: <a className="App-link" href={data.blog} target="_blank">{data.blog}</a></td></tr>
+        </table>
+        <p></p>
+        <p></p>
+        <spam>Tem interesse em aprender mais sobre a tecnologia utilizada?</spam>
+        <h5>
+        <a className="App-link" href="https://docs.google.com/forms/d/e/1FAIpQLSfhPSSuCXRZFAoxf4EZDmRoCZVsreYg8uDeLfZwz0x9cTt8pQ/viewform" target="_blank">
+          Inscreva-se 
+        </a> na Oficina React
+        </h5>
+      </div>
     </div>
   );
 }
-
-export default App;
+ 
